eccc214100eae3aaa2fbbadce1e60a9cfd476335
alirezabashi98 <alirezabashi98@gmail.com>
2021-01-28 20:44:42 +0330

    Normal mode without injection dependence with dagger

--- app/src/main/java/com/example/dagger2/MainActivity.kt
+++ app/src/main/java/com/example/dagger2/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.dagger2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mobile = Mobile(
+            Board(Storage(), Camera()),
+            Screen(Lcd(), Touch())
+        )
+
     }
 }
